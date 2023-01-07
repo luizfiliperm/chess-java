a4f6815c6a07b9abb5c916f88fd2d5f1fc3de15e
Luiz Filipe <luizfilipemr17@gmail.com>
2023-01-07 14:28:47 -0300

    Adding BoardException Class and adding defensive progamming on Board Class

--- src/models/boardgame/Board.java
+++ src/models/boardgame/Board.java
@@ -6,11 +6,15 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(Integer rows, Integer columns) {
+        if(rows < 1 || columns < 1){
+            throw new BoardException("Error at creating board! There must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
     }
-    
+
+
     public Integer getRows(){
         return rows;
     }
@@ -20,15 +24,40 @@ public class Board {
     }
     
     public Piece piece(Integer row, Integer column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Position not in the board");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not in the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("Already exists a piece in position " + position);
+        }
+
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(Integer row, Integer column){
+        return row >= 0 && row < rows && column >=0 &&  column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(positionExists(position)){
+            throw new BoardException("Position not in the board");
+        }
+        return piece(position) != null;
     }
 }
---
+++ src/models/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package models.boardgame;
+
+public class BoardException extends RuntimeException{
+    
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+}
