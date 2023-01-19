2aa6ae9931f9b7589913e2528eb2ecb83f3dc55b
Luiz Filipe <luizfilipemr17@gmail.com>
2023-01-19 12:06:59 -0300

    Adding moveCount to Pieces

--- src/models/chess/ChessPiece.java
+++ src/models/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import models.boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private Integer moveCount;
     
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -15,6 +16,18 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    public Integer getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        moveCount--;
     }
 
     public ChessPosition getChessPosition(){
